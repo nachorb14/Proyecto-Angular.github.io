c7bc4c63967576fb25476982735df7a21efd30d5
Ignacio Romero Barolin <nachorb2001@gmail.com>
2024-03-19 01:32:09 -0300

    Primer versión del proyecto sin terminar la entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AsideNavComponent } from './aside-nav/aside-nav.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AsideNavComponent,
+    StudentsPageComponent
   ],
   imports: [
     BrowserModule,
